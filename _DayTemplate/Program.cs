3af4106cd84bf8f99f7527b9bb859cde21b59850
Sindarin27 <sindarin27@gmail.com>
2021-12-02 11:46:39 +0100

    Add a template so I can easily start a new day

---
+++ _DayTemplate/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using static Utils.Program;
+
+namespace _DayTemplate
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            // Read input
+            ForEachInputLine(input =>
+            {
+                // Do stuff with input
+            });
+            
+            // Do stuff for part 1
+            Console.WriteLine($"");
+            
+            // Do stuff for part 2
+            Console.WriteLine($"");
+        }
+    }
+}
