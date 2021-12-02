6ecfcbf301bab05ecc637cbf84d881220fc91cce
Sindarin27 <sindarin27@gmail.com>
2021-12-02 11:08:24 +0100

    Complete day 2, add Utility methods

---
+++ Day2/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security;
+using Utils;
+
+namespace Day2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // First, read the input into an understandable format
+            List<int> horizontalChanges = new List<int>();
+            List<int> verticalChanges = new List<int>();
+
+            Utils.Program.ForEachInputLine(input =>
+            {
+                string[] words = input.Split(' ');
+                string direction = words[0];
+                int amount = int.Parse(words[1]);
+
+                switch (direction)
+                {
+                    case "forward":
+                        horizontalChanges.Add(amount);
+                        verticalChanges.Add(0);
+                        break;
+                    case "up":
+                        horizontalChanges.Add(0);
+                        verticalChanges.Add(-amount);
+                        break;
+                    case "down":
+                        horizontalChanges.Add(0);
+                        verticalChanges.Add(amount);
+                        break;
+                }
+            });
+            
+            // Calculate the final position
+            int finalHorizontal = horizontalChanges.Sum();
+            int finalVertical = verticalChanges.Sum();
+            
+            // Give answer to part 1
+            Console.WriteLine($"Horizontal position * vertical depth = {finalHorizontal * finalVertical}");
+            
+            // Start calculating part 2
+            // Horizontal position calculation stays the same
+
+            IEnumerable<int> aim = verticalChanges.Scan((change, prevAim) => prevAim + change, 0);
+            IEnumerable<int> aimVerticalChanges = horizontalChanges.Zip(aim, (horizontalChange, currentAim) => horizontalChange * currentAim);
+            int finalAimVertical = aimVerticalChanges.Sum();
+            
+            Console.WriteLine($"Using Aim: Horizontal position * vertical depth = {finalHorizontal * finalAimVertical}");
+        }
+    }
+}
---
+++ Utils/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Utils
+{
+    public static class Program
+    {
+        private static void Main()
+        {
+            Console.WriteLine("You probably shouldn't run this on its own...");
+        }
+        public static IEnumerable<T> ForEachInputLine<T>(Func<String, T> inputToOutput)
+        {
+            for (string input; (input = Console.ReadLine() ?? "") != "";)
+            {
+                yield return inputToOutput(input);
+            }
+        }
+        public static void ForEachInputLine(Action<String> handleInput)
+        {
+            for (string input; (input = Console.ReadLine() ?? "") != "";)
+            {
+                handleInput(input);
+            }
+        }
+        
+        public static IEnumerable<U> Scan<T, U>(this IEnumerable<T> input, Func<U, T, U> next, U state) {
+            yield return state;
+            foreach(var item in input) {
+                state = next(state, item);
+                yield return state;
+            }
+        }
+    }
+}
