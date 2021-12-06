109e3fb4ab710a27688157ecf44825ee976f3555
Sindarin27 <sindarin27@gmail.com>
2021-12-06 15:41:22 +0100

    Day 6. Easy. Totally didn't get an OverflowException on my first attempt.

---
+++ Day6/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using static Utils.Program;
+
+namespace Day6
+{
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+            // Read input
+            long[] fishes = ReadInitialFish();
+            long fishesOnDay7 = 0, fishesOnDay8 = 0;
+
+            for (int day = 0; day < 256; day++)
+            {
+                long fishesOnDay6 = fishesOnDay7;
+                fishesOnDay7 = fishesOnDay8;
+                fishesOnDay8 = fishes[day % 7];
+                fishes[day % 7] += fishesOnDay6;
+                // Print answer to part 1
+                if (day == 79) Console.WriteLine($"On day 80, there are {fishes.Sum() + fishesOnDay7 + fishesOnDay8} fish in total");
+
+            }
+            
+            Console.WriteLine($"After 256 days, there are a  whopping {fishes.Sum() + fishesOnDay7 + fishesOnDay8} fish in total!");
+        }
+
+        private static long[] ReadInitialFish()
+        {
+            long[] fishes = new long[7];
+            IEnumerable<byte> input = Console.ReadLine()!.Split(',').Select(byte.Parse);
+            foreach (byte fish in input)
+            {
+                fishes[fish]++;
+            }
+
+            return fishes;
+        }
+    }
+}
