f4e70276130c633cb90bbee44f47a5b25aa90f51
Sindarin27 <sindarin27@gmail.com>
2021-12-09 12:33:00 +0100

    Flex my IBV knowledge and complete day 9

---
+++ Day9/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using static Utils.Program;
+
+namespace Day9
+{
+    internal static class Program
+    {
+        /// Offsets to check for each position
+        private static readonly (int x, int y)[] Offsets = {(1, 0), (-1, 0), (0, 1), (0, -1)};
+        private static void Main(string[] args)
+        {
+            // Read input
+            byte[,] map = CreateRectangularArray(ForEachInputLine(input =>
+            {
+                return input.ToCharArray().Select(x => byte.Parse(x.ToString())).ToArray();
+            }).ToList());
+
+            // Find sum of all local minima
+            List<(int x, int y)> localMinima = FindLocalMinima(map, Offsets).ToList();
+            int sumOfLocalMinima = localMinima.Select(coords => map[coords.x, coords.y] + 1).Sum();
+            Console.WriteLine($"Sum of risk levels = {sumOfLocalMinima} (with {localMinima.Count} lowest points)");
+            
+            // Find all basin sizes
+            List<int> sizes = localMinima.Select(coords => FindBasinSize(map, coords, Offsets)).ToList();
+            sizes.Sort(); sizes.Reverse(); // Sort descending
+            int productOfBiggestThreeBasins = sizes.Take(3).Aggregate<int>((x, y) => x * y);
+            Console.WriteLine($"The three largest basins have a total area of {productOfBiggestThreeBasins}");
+        }
+
+        /// <summary>
+        /// Find all local minima in the array according to the provided list of neighbours
+        /// </summary>
+        /// <param name="array">Array to find local minima in</param>
+        /// <param name="offsets">List of offsets for neighbours</param>
+        /// <returns>All coordinates of local minima inside the array</returns>
+        private static IEnumerable<(int x, int y)> FindLocalMinima(byte[,] array, IList<(int x, int y)> offsets)
+        {
+            for (int x = 0; x < array.GetLength(0); x++) for (int y = 0; y < array.GetLength(1); y++)
+            {
+                byte value = array[x, y];
+                // If no neighbour exists for which the value is equal or default, this is a local minima.
+                if (!offsets.Any(offset => value >= array.GetIndexOrDefault(x + offset.x, y + offset.y, byte.MaxValue))) 
+                    yield return (x, y);
+            }
+        }
+
+        // Find the size of a basin starting at the lowest point
+        private static int FindBasinSize(byte[,] array, (int x, int y) lowestPoint, IList<(int x, int y)> offsets)
+        {
+            // Perform flood fill on the basin to get the size
+            Queue<(int x, int y)> front = new();
+            HashSet<(int x, int y)> seen = new();
+            front.Enqueue(lowestPoint);
+            seen.Add(lowestPoint);
+            int size = 0;
+
+            while (front.Count > 0)
+            {
+                (int x, int y) = front.Dequeue();
+                if (array.IsOutOfBounds(x, y)) continue;
+                if (array[x, y] == 9) continue;
+                size++;
+                foreach ((int xOffset, int yOffset) in offsets)
+                {
+                    (int x, int y) coords = (x + xOffset, y + yOffset);
+                    if (seen.Contains(coords)) continue;
+                    front.Enqueue(coords);
+                    seen.Add(coords);
+                }
+            }
+
+            return size;
+        }
+    }
+}
--- Utils/Program.cs
+++ Utils/Program.cs
@@ -57,5 +57,62 @@ namespace Utils
 
             return counts;
         }
+        
+        /// <summary>
+        /// From a list of 1d arrays, create a 2d array
+        /// From https://stackoverflow.com/a/9775057
+        /// </summary>
+        /// <param name="arrays">List of 1D arrays</param>
+        /// <typeparam name="T">Type of data in array</typeparam>
+        /// <returns>2D array</returns>
+        /// <exception cref="ArgumentException">Arguments are invalid, for example when not all arrays have the same length</exception>
+        public static T[,] CreateRectangularArray<T>(IList<T[]> arrays)
+        {
+            if (arrays.Count == 0) return new T[0, 0]; 
+            int minorLength = arrays[0].Length;
+            T[,] ret = new T[arrays.Count, minorLength];
+            for (int i = 0; i < arrays.Count; i++)
+            {
+                T[] array = arrays[i];
+                if (array.Length != minorLength)
+                {
+                    throw new ArgumentException
+                        ("All arrays must be the same length");
+                }
+                for (int j = 0; j < minorLength; j++)
+                {
+                    ret[i, j] = array[j];
+                }
+            }
+            return ret;
+        }
+
+        /// <summary>
+        /// Get value at the index of 2D array, return default value if index is out of bounds
+        /// </summary>
+        /// <param name="array">Array to get value out of</param>
+        /// <param name="x">First coordinate of index</param>
+        /// <param name="y">Second coordinate of index</param>
+        /// <param name="defaultValue">Value to return when index is out of bounds</param>
+        /// <typeparam name="T">Type of values</typeparam>
+        /// <returns>Item at array[x,y] if inside bounds, defaultValue if out of bounds</returns>
+        public static T GetIndexOrDefault<T>(this T[,] array, int x, int y, T defaultValue)
+        {
+            if (IsOutOfBounds(array, x, y)) return defaultValue;
+            return array[x, y];
+        }
+
+        /// <summary>
+        /// Check whether the given index is inside the bounds of the array
+        /// </summary>
+        /// <param name="array"></param>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        /// <typeparam name="T"></typeparam>
+        /// <returns></returns>
+        public static bool IsOutOfBounds<T>(this T[,] array, int x, int y)
+        {
+            return x < 0 || x >= array.GetLength(0) || y < 0 || y >= array.GetLength(1);
+        }
     }
 }
