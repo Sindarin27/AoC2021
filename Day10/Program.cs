7b5fb4dc2fcf28d4a5eaa1bed24730360b43a858
Sindarin27 <sindarin27@gmail.com>
2021-12-10 09:14:35 +0100

    Rename all "dayX" to "dayXX" because stuff is dumb and sorts only on the first number, so day10 would come after day1 and before day2.

---
+++ Day10/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using static Utils.Program;
+
+namespace Day10
+{
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+            // Read input
+            ForEachInputLine(input =>
+            {
+                // TODO Do stuff with input
+            });
+
+            // TODO Do stuff for part 1
+            Console.WriteLine($"");
+
+            // TODO Do stuff for part 2
+            Console.WriteLine($"");
+        }
+    }
+}
